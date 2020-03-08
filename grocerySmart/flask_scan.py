da3b4e2430aae513f2664ffd0def7399e09d23f0
erinmacfarlane <erinmacfarlane@icloud.com>
2020-03-08 16:37:59 -0400

    Html and CSS Files

---
+++ grocerySmart/flask_scan.py
@@ -0,0 +1,26 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+@app.route('/scan')
+def scanpage():
+	return render_template('scan.html')
+
+@app.route('/fridge')
+def foodAvailable():
+	return render_template('fridge.html')
+
+@app.route('/notifications')
+def notifations():
+	return render_template('notifications.html')
+
+@app.route('/foodConsumption')
+def history():
+	return render_template('foodConsumption.html')
+
+if __name__ == '__main__':
+	app.run()
